6bd49707b23396b8e8d8ad1a12f14af8438cafb5
Chanel Morgan <chanelmorgan05@icloud.com>
2024-01-15 20:43:28 +0000

    creating the email section

---
+++ portfolio-website/app/components/EmailSection.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import GithubIcon from '/Users/chanelmorgan/PortfolioWebsite/portfolio-website/public/images/github-icon.svg'; 
+import LinkedinIcon from '/Users/chanelmorgan/PortfolioWebsite/portfolio-website/public/images/linkedin-icon.svg'; 
+import Link from 'next/link'; 
+import Image from 'next/image';
+
+const EmailSection = () => { 
+    return ( 
+        <section className= "grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+
+        <div>
+            <h5 className="text-xl font-bold text-white my-2 ">
+                Let's Connect!
+            </h5>
+            <p className="text-[#ADB7BE] mb-4 mac-w-md">
+                {" "}
+                I am currently looking for new opportunities, my inbox is always open. 
+                Whether you have a question or just want to say hi, I will try my best to get back to 
+                you! - change this 
+            </p> 
+            <div className="socials flex flex-row gap-2"></div>
+                <Link href="github.com">
+                    <Image src={GithubIcon} alt="Github Icon" /> 
+                </Link>
+                <Link href="linkedin.com">
+                    <Image src={LinkedinIcon} alt="Linkedin Icon" /> 
+                </Link>
+        </div> 
+
+        <div>
+            <form className="flex flex-col">
+                <div className="mb-6">
+                <label 
+                htmlFor="email" 
+                className="text-white block mb-2 text-sm font-medium">Your email</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    required 
+                    className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    placeholder="chanel@gmail.com"
+                />
+                </div> 
+                <div className="mb-6">
+                <label 
+                htmlFor="subject" 
+                className="text-white block mb-2 text-sm font-medium">Subject</label>
+                <input 
+                    type="text" 
+                    id="subject" 
+                    required 
+                    className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                    placeholder="Just saying hi"
+                />
+                </div>
+                <div className="mb-6">
+                    <label
+                    htmlFor="message"
+                    className="text-white block text-sm mb-3 font-medium"
+                    >
+                        Message 
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        className="bg-[#18191E] border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                        placeholder="Let's talk about...."
+                    />
+                </div>
+                <button 
+                    type="submit"
+                    className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
+                        Submit</button>
+
+                
+            </form> 
+
+        </div>
+        </section>
+
+    );
+}; 
+export default EmailSection;
--- portfolio-website/app/page.js
+++ portfolio-website/app/page.js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import HeroSection from './components/HeroSection';
 import NavBar from './components/NavBar';
 import AboutSection from './components/AboutSection';
+import ProjectSection from './components/ProjectSection';
+import EmailSection from './components/EmailSection';
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
     <div className="container mt-24 mx-auto px-12 py-4"> 
     <HeroSection /> 
     <AboutSection /> 
+    <ProjectSection /> 
+    <EmailSection /> 
     </div>
     </main>
   )
